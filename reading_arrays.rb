aa97bcbcac462b4d018c59b61d447ab432aaaf06
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-25 15:18:08 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -33,7 +33,7 @@ def last_student_by_method
 end
 
 def first_second_and_third_students
-  STUDENT_NAMES123 = [STUDENT_NAMES[0],
+  array123 = [STUDENT_NAMES[0],
   STUDENT_NAMES[1],
   STUDENT_NAMES[2]]
   # Write a solution that returns the first, second and third students
